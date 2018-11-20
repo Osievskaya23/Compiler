55ad1df9aa194c9620290ff11ec3c85360c56379
Valentina Osievskaya <valentinaosievskaya@gmail.com>
2018-11-20 16:43:20 +0200

    syntactic commit

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 public class LexicalAnalyzer extends Main{
 
     /** Array, what contain token lexem from the "text.txt" file and information about it */
-    public ArrayList<Token> tokens = new ArrayList<>();
+    ArrayList<Token> tokens = new ArrayList<>();
 
     /** Array, what contains key words from the "key words.txt" file */
-    public ArrayList<String> keyWords = new ArrayList<>();
+    private ArrayList<String> keyWords = new ArrayList<>();
 
     /** Text to by analyzed*/
-    public String inputText;
+    private String inputText;
 
-    /**
-     * Default constructor
-     */
-    public LexicalAnalyzer(){
+    /** Default constructor */
+    LexicalAnalyzer(){
     }
 
     /**
@@ -23,7 +21,7 @@ public class LexicalAnalyzer extends Main{
      * Read text to be analyzed
      * @return text to be analyzed
      */
-    public String getInputText (){
+    String getInputText (){
         try{
             inputText = "";
             FileInputStream fstream = new FileInputStream("C:\\Users\\38066\\IdeaProjects\\SP_lab3_Osievskaya\\src\\text.txt");
@@ -46,7 +44,7 @@ public class LexicalAnalyzer extends Main{
      * Read key words
      * @return list of key words
      */
-    public ArrayList<String> getKeyWords (){
+    ArrayList<String> getKeyWords (){
         try{
             FileInputStream fstream = new FileInputStream("C:\\Users\\38066\\IdeaProjects\\SP_lab3_Osievskaya\\src\\key words.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
@@ -67,7 +65,7 @@ public class LexicalAnalyzer extends Main{
      * @param tokens is ArrayList of Tokens for sorting
      * @return sorted tokens
      */
-    public ArrayList<Token> sortToken(ArrayList<Token> tokens){
+    ArrayList<Token> sortToken(ArrayList<Token> tokens){
         int length = tokens.size();
         for(int i = 0; i < length - 1; i++){
             for (int j = 0; j < length - i - 1; j++) {
